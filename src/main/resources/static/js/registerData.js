eb08df5397218835dec6e357ae4db1fa357f9184
demo <2297802467@qq.com>
2020-05-26 11:52:35 +0800

    js

---
+++ src/main/resources/static/js/registerData.js
@@ -0,0 +1,17 @@
+new Vue({
+    el: "#app",
+    data: {
+        userName: '',
+        phone: '',
+        password: ''
+    },
+    dataList: [],
+    methods: {
+
+        register: function () {
+            var _this = this;
+            axios.post()
+
+        }
+    }
+});
