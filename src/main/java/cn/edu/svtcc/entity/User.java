0b2788696f9460086fa85644d701f60f48071157
demo <2297802467@qq.com>
2020-06-05 22:19:21 +0800

    User.java

--- src/main/java/cn/edu/svtcc/entity/User.java
+++ src/main/java/cn/edu/svtcc/entity/User.java
@@ -19,18 +19,47 @@ import java.io.Serializable;
  * @date 2020/05/25/21:37
  */
 @Component
-@EqualsAndHashCode(exclude = {"passWord", "phoneNumber", "userName"})
+@EqualsAndHashCode(exclude = {"passWord", "phoneNumber", "userName", "email", "role", "address"})
 @ToString
 @NoArgsConstructor
 @AllArgsConstructor
 @Setter
 @Getter
 @Slf4j
+
 public class User implements Serializable {
+    /**
+     * userId
+     */
     private Integer userId;
+    /**
+     * userName
+     */
     private String userName;
+    /**
+     * passWord
+     */
     private String passWord;
+    /**
+     * phoneNumber
+     */
     private String phoneNumber;
+    /**
+     * age
+     */
+    private Integer age;
+    /**
+     * email
+     */
+    private String email;
+    /**
+     * address
+     */
+    private String address;
+    /**
+     * role
+     */
+    private String role;
 
     private static final long serialVersionUID = 1L;
 
